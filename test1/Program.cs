6dba370de85425d1f4368d30991b50af1448e8b5
Ruben Gomez <rugolop@gmail.com>
2024-09-10 11:24:43 +0200

    Second commit

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+Console.Write("Congratulations");
+Console.Write(" ");
+Console.Write("You Wrote your first lines of Code");
